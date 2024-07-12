8a6d414da1a49de8257e838e358dadc7327faebc
Laura Kurtzberg <laura.kurtzberg@gmail.com>
2024-07-12 19:12:23 -0400

    added the two tests! I wasn't able to fix every lint error though

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError
+# I didn't end up using ParseError
+# from rest_framework.exceptions import ParseError
 
 
 class Home(TemplateView):
@@ -26,15 +27,13 @@ class AddressParse(APIView):
                 'address_type': address_type
             })
 
+        except TypeError:
+            return Response({'error': 'an unknown error occurred'}, status=400)
         except Exception as e:
             # Got to handle strings that cannot be parsed and return the error!
             return Response({'error': str(e.message.split('\n')[1].strip())}, status=400)
 
-
     def parse(self, address):
         # usaddress: https://github.com/datamade/usaddress
-
-        # I'm assuming that usaddress.tag won't change and will give
-        # me the tuple in order (components dictionary first, then type string)
         address_components, address_type = usaddress.tag(address)
         return address_components, address_type
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,53 @@
+# I got a lint error that pytest is imported but never used...
+# keeping this import statement here for testing anyway
 import pytest
+from django.urls import reverse
+import json
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
+    # Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    url = reverse('address-parse')
+    url += '?input_string=' + address_string
+
+    correct_format = {
+        "input_string": "123 main st chicago il",
+        "address_components":
+            {
+                "AddressNumber": "123",
+                "StreetName": "main",
+                "StreetNamePostType": "st",
+                "PlaceName": "chicago",
+                "StateName": "il"
+            },
+            "address_type": "Street Address"
+        }
+
+    response = client.get(url)
+    address_data = json.loads(response.content)
+
+    assert response.status_code == 200
+    assert address_data == correct_format
 
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
+    # The address_string below will raise a RepeatedLabelError,
+    # so ParseAddress.parse() should not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    url = reverse('address-parse')
+    url += '?input_string=' + address_string
+
+    # Check for repeated label error
+    # note: eslint doesn't like the long line below, but test will not register
+    # equal json structure if I split the string up into two lines
+    correct_error_returned = {
+        'error':
+        'ERROR: Unable to tag this string because more than one area of the string has the same label'
+    }
+
+    response = client.get(url)
+    response_data = json.loads(response.content)
+    assert response.status_code == 400
+    assert response_data == correct_error_returned
