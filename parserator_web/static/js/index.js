c0fca4f8ee0df4a917c87b5fda1524d95b2b59e4
Laura Kurtzberg <laura.kurtzberg@gmail.com>
2024-07-11 21:23:57 -0400

    added some function ideas for working with the address inputed into the form and making sure it isn't empty

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,7 +1,7 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
 // TODO: should I leave a default value on the inputString?
-async function getData(inputString='123%20main%20st%20chicago%20il') {
+async function getData(inputString) {
     const url = "api/parse/?input_string=" + inputString;
     const response = await fetch(url, {
        method: "GET",
@@ -12,11 +12,40 @@ async function getData(inputString='123%20main%20st%20chicago%20il') {
      });
  }
 
-// I'm going to override the default submit action for the 'Parse!' button
- document.getElementById("submit").addEventListener("submit", function (e) {
-   e.preventDefault();
+ // a function to check if the address is not empty
+ function addressIsNotEmpty(address) {
+   if (!addressString) {
+     console.error('Address field is empty, cannot parse');
+     return false;
+   } else {
+     return true;
+   }
  }
 
-getData();
+ // display the error message to the UI if address is empty
+ function displayEmptyError() {
+   // TODO: add function body
+ }
+
+// I'm going to override the default submit action for the 'Parse!' button
+ document.getElementById("addressform").addEventListener("submit", function (e) {
+   e.preventDefault();
+
+   // I was debating whether to use document.getElementById("address").value;
+   // but I think FormData accomodates a situation where more fields might
+   // be gathered in the future, or in case the id of element changes
+   const formData = new FormData(e.target);
+   const formProps = Object.fromEntries(formData);
+   const addressString = formProps.address;
+
+   // check for empty address
+   if addressIsNotEmpty(addressString) {
+     getData();
+   } else {
+     displayEmptyError();
+   }
+ });
+
+//getData();
 // replace spaces with %20 in url request with fetch
 // for example: .../api/parse/?input_string='123%20n%20mai'
